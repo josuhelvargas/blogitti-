1ad5147b3a2832c64e09a57b9572b752d3d91f8c
d4v1nc1 vargas <josuhelvargas@gmail.com>
2020-03-02 05:55:02 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+
+<script>
+	import Home from './Home.svelte';
+	import Login from './Login.svelte';
+	import NavigationTop from './NavigationTop.svelte';
+</script>
+
+
+
+<main>
+<NavigationTop></NavigationTop>
+<br/>
+<Login></Login>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 2em;
+		max-width: 80px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+ 
---
+++ src/Home.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+
+<main>
+    <div>
+     Praetorian Programming
+    </div>
+</main>
+
+
+
+<style>
+    div {
+        color:white;
+        font-size: 2rem;
+       font-family: 'Orbitron', sans-serif;
+    }
+</style>
+
